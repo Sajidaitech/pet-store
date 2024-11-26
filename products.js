6744247b6fb7cd4ce92b7b67ba09e2194fe61d37
Sajidaitech <sajidaitech@gmail.com>
2024-11-26 15:38:43 +0400

    Add files via upload

---
+++ products.js
@@ -0,0 +1,47 @@
+let products = [
+    {
+"id": 1001,
+"title": "Cat Food, 2516 bag",
+"description": "A 25 pound bag of ‹em›irresistible‹/em›, organic goodness for your cat.",
+"price": 10.00,
+"image": "images/image.png",
+"availableInventory": 10,
+"rating": 2
+    },
+{
+"id": 1002,
+"title": "Cat Food, 2516 bag",
+"description": "A 25 pound bag of ‹em›irresistible‹/em›, organic goodness for your cat.",
+"price": 20.00,
+"image": "images/image2.png",
+"availableInventory": 10,
+"rating": 2
+    },
+    {
+    "id": 1003,
+"title": "Cat Food, 2516 bag",
+"description": "A 25 pound bag of ‹em›irresistible‹/em›, organic goodness for your cat.",
+"price": 30.00,
+"image": "images/image3.png",
+"availableInventory": 10,
+"rating": 2
+    },
+    {
+    "id": 1004,
+"title": "Cat Food, 2516 bag",
+"description": "A 25 pound bag of ‹em›irresistible‹/em›, organic goodness for your cat.",
+"price": 50.00,
+"image": "images/image4.png",
+"availableInventory": 10,
+"rating": 2
+    },
+    {
+        "id": 1005,
+    "title": "Cat Food, 2516 bag",
+    "description": "A 25 pound bag of ‹em›irresistible‹/em›, organic goodness for your cat.",
+    "price": 90.00,
+    "image": "images/image5.png",
+    "availableInventory": 10,
+    "rating": 2
+        }
+]
